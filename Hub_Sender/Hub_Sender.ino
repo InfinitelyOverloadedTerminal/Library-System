58e69dab2958d9f2186f8b73fb478f679d1aae73
Gabriel Turmail <gabriel.turmail@gmail.com>
2023-11-02 11:35:18 -0400

    Initial commit

---
+++ Hub_Sender/Hub_Sender.ino
@@ -0,0 +1,12 @@
+#include <esp_now.h> //Needed for ESPNow
+#include <WiFi.h> //Needed for ESPNow
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
